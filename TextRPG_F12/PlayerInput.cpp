f36f42bea8b3b1fec63b6c4551a4934d05ae41c4
9yutae <9yutae@gmail.com>
2025-01-13 20:12:04 +0900

    Update: PlayerInput 추가

--- TextRPG_F12/PlayerInput.cpp
+++ TextRPG_F12/PlayerInput.cpp
@@ -13,3 +13,17 @@ int PlayerInput::getPlayerChoice()
     std::cin >> choice;
     return choice;
 }
+
+int PlayerInput::getPlayerChoiceAfterBattle()
+{
+    std::cout << "축하합니다! 전투에서 승리하셨습니다!\n";
+    std::cout << "다음 행동을 선택해 주세요.\n";
+    std::cout << "1. 다음 라운드 진행!\n";
+    std::cout << "2. 상점 방문\n";
+    std::cout << "Enter your choice: ";
+
+    int choice;
+    std::cin >> choice;
+
+    return choice;
+}
