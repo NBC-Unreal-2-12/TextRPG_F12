dbda3b725300f8ba79227c4ba2554e57f4aa8ef5
Xotepsin <167324082+Xotepsin@users.noreply.github.com>
2025-01-13 13:49:36 +0900

    유니코드 서명인코딩

--- TextRPG_F12/Skill.h
+++ TextRPG_F12/Skill.h
@@ -1,4 +1,4 @@
-#ifndef SKILL_H
+﻿#ifndef SKILL_H
 #define SKILL_H
 #pragma once
 
@@ -18,11 +18,16 @@ public:
     Skill(const string& name, int manaCost, double damageFactor)
         : skillName(name), manaCost(manaCost), damageFactor(damageFactor) {
     }
-    virtual void useSkill() const = 0; // 스킬 사용
     virtual string getSkillName() const = 0; // 스킬 이름
-    virtual ~Skill() 
-    {
-    }
+    virtual ~Skill(){}
+	int getManaCost() const
+	{
+		return manaCost;
+	}
+	double getDamageFactor() const
+	{
+		return damageFactor;
+	}
 };
 
 
@@ -30,12 +35,8 @@ class WarriorSkill : public Skill
 {
 public:
     WarriorSkill()
-        : Skill("전사의 강타", 10, 2.0f) {
+        : Skill("전사의 강타", 10, 2.0) {
     } // 이름, 고정 MP 소모, 고정 데미지 배율
-    void useSkill() const override 
-    {
-        cout << "전사의 강타를 사용합니다!" << endl;
-    }
 
     string getSkillName() const override 
     {
@@ -46,10 +47,10 @@ public:
 class MageSkill : public Skill 
 {
 public:
-    void useSkill() const override 
-    {
-        cout << "마법사의 화염구를 사용합니다!" << endl;
-    }
+	MageSkill()
+		: Skill("마법사의 화염구", 20, 1.5) {
+	} // 이름, 고정 MP 소모, 고정 데미지 배율
+
 
     string getSkillName() const override 
     {
@@ -60,10 +61,10 @@ public:
 class ArcherSkill : public Skill
 {
 public:
-    void useSkill() const override 
-    {
-        cout << "궁수의 연속 사격을 사용합니다!" << endl;
-    }
+	ArcherSkill()
+		: Skill("궁수의 연속 사격", 15, 1.8) {
+	} // 이름, 고정 MP 소모, 고정 데미지 배율
+
     string getSkillName() const override
     {
         return "궁수의 연속 사격";
